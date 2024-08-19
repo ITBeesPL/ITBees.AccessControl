9e47715cf692c058feb0b06fa0889fbeb6add923
Jakub Zohar Rutkowski <kuba@skb.pl>
2024-08-19 15:09:09 +0200

    Add project files.

---
+++ ITBees.AccessControl/AccessControlSetup.cs
@@ -0,0 +1,17 @@
+﻿
+using ITBees.FAS.Setup;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ITBees.AccessControl
+{
+    public class AccessControlSetup : IFasDependencyRegistrationWithGenerics
+    {
+        public void Register<TContext, TIdentityUser>(IServiceCollection services, IConfigurationRoot configurationRoot) where TContext : DbContext where TIdentityUser : IdentityUser, new()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
