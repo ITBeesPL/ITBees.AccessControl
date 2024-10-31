c9107cd6c04c940ba24a0e0804a4bd989a1844f1
Jakub Zohar Rutkowski <kuba@skb.pl>
2024-10-31 16:00:52 +0100

    AcGroupsAccessCards support

---
+++ ITBees.AccessControl/Controllers/PlatformOperator/AcGroupsAccessCardsController.cs
@@ -0,0 +1,25 @@
+﻿using ITBees.AccessControl.Services.PlatformOperator;
+using ITBees.Interfaces.Repository;
+using ITBees.RestfulApiControllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ITBees.AccessControl.Controllers.PlatformOperator;
+
+public class AcGroupsAccessCardsController : RestfulControllerBase<AcGroupsAccessCardsController>
+{
+    private readonly IAcGroupAccessCardsService _acGroupAccessCardsService;
+
+    public AcGroupsAccessCardsController(ILogger<AcGroupsAccessCardsController> logger,
+        IAcGroupAccessCardsService acGroupAccessCardsService) : base(logger)
+    {
+        _acGroupAccessCardsService = acGroupAccessCardsService;
+    }
+
+    [HttpGet]
+    [Produces<PaginatedResult<AcGroupAccessCardsVm>>]
+    public IActionResult Get(int? page, int? pageSize, string? sortColumn, SortOrder? sortOrder)
+    {
+        return ReturnOkResult(() => { _acGroupAccessCardsService.GetAll(page, pageSize, sortColumn, sortOrder); });
+    }
+}
