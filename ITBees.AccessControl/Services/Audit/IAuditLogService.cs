29eb29c81091ac18a62dc705a44f356995a929fe
Kacper <fastrabbiton@gmail.com>
2025-11-05 12:36:20 +0100

    return auditLog id

--- ITBees.AccessControl/Services/Audit/IAuditLogService.cs
+++ ITBees.AccessControl/Services/Audit/IAuditLogService.cs
@@ -2,6 +2,6 @@ namespace ITBees.AccessControl.Services.Audit;
 
 public interface IAuditLogService
 {
-    void SaveAuditLog(Guid? userAccountGuid, Guid? companyGuid, int auditLogTypeId, string action, string description, string entityName,
+    int SaveAuditLog(Guid? userAccountGuid, Guid? companyGuid, int auditLogTypeId, string action, string description, string entityName,
         int? entityId, Guid? entityGuid);
 }
